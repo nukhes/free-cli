8262f45e1977fa4a5201deb5aa38736110cb908d
nukhes <nukhes@protonmail.com>
2024-01-03 12:58:58 -0300

    ef

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ class Program
             freeN = Convert.ToDecimal(free, CultureInfo.InvariantCulture);
 
             Console.WriteLine("\n{0, 20} {1, 10} {2, 10}", "total", "used", "free");
-            Console.WriteLine("Mem: {0, 13:N0}GB {1, 8:N0}GB {2, 8:N0}GB", totN/1024, (totN - freeN)/1024, freeN/1024);
+            Console.WriteLine("Mem: {0, 13:N0}GB {1, 8:N0}GB {2, 8:N0}GB", totN / 1024, (totN - freeN) / 1024, freeN / 1024);
 
             Console.ReadLine();
         }
